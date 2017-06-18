1f1884c30631546474428c0cfa15d5e46e3e30c0
jonas <Jonas_Falk.Jakobsen@Student.Reutlingen-University.DE>
2017-06-18 02:49:38 +0200

    listview showing data from REST API

--- app/src/main/java/com/vogella/android/tap4share/MainActivity.java
+++ app/src/main/java/com/vogella/android/tap4share/MainActivity.java
@@ -3,23 +3,52 @@ package com.vogella.android.tap4share;
 
 import android.app.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+//    API
+///???
+private String TAG = MainActivity.class.getSimpleName();
 
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    // URL to get contacts JSON
+//    private static String url = "http://api.androidhive.info/contacts/";
+    private static String url = "http://141.70.55.157:8080/api/artists/";
+
+    ArrayList<HashMap<String, String>> contactList;
+
+
+
+    //other
     private TextView tabHome;
     private TextView tabTap;
     ImageView image;
@@ -38,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bindView();
-
 
         TextView photoButton = (TextView) this.findViewById(R.id.text_tap);
         this.imageView=(ImageView) this.findViewById(R.id.imageView1);
@@ -65,6 +93,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         System.out.println("HERE IS " + imageView);
+
+
+
+        //API
+        contactList = new ArrayList<>();
+
+        lv = (ListView) findViewById(R.id.list);
+
+        //Get JSON (I think)
+        new GetContacts().execute();
     }
 
 
@@ -149,5 +187,114 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
     }
+
+
+
+
+//    API CLASS
+private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        // Showing progress dialog
+        pDialog = new ProgressDialog(MainActivity.this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        HttpHandler sh = new HttpHandler();
+
+        // Making a request to url and getting response
+        String jsonStr = sh.makeServiceCall(url);
+
+        Log.e(TAG, "Response from url: " + jsonStr);
+
+        if (jsonStr != null) {
+            try {
+                JSONArray jsonObj = new JSONArray(jsonStr);
+
+                // Getting JSON Array node
+//                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+
+//                    // looping through All Contacts
+                for (int i = 0; i < jsonObj.length(); i++) {
+
+                    JSONObject c = jsonObj.getJSONObject(i);
+                    System.out.println("meh count" + c);
+
+                    String id = c.getString("id");
+                    String name = c.getString("name");
+                    String birthPlace = c.getString("birthPlace");
+                    String birthDate = c.getString("birthDate");
+                    String favoritebool = c.getString("favoritebool");
+
+                    // tmp hash map for single contact
+                    HashMap<String, String> contact = new HashMap<>();
+
+                    // adding each child node to HashMap key => value
+                    contact.put("id", id);
+                    contact.put("name", name);
+                    contact.put("birthPlace", birthPlace);
+                    contact.put("birthDate", birthDate);
+                    contact.put("favoritebool", favoritebool);
+
+                    // adding contact to contact list
+                    contactList.add(contact);
+                }
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Json parsing error: " + e.getMessage(),
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+        } else {
+            Log.e(TAG, "Couldn't get json from server.");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            "Couldn't get json from server. Check LogCat for possible errors!",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            });
+
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        System.out.println("postexecute");
+        super.onPostExecute(result);
+        // Dismiss the progress dialog
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+        /**
+         * Updating parsed JSON data into ListView
+         * */
+        ListAdapter adapter = new SimpleAdapter(
+                MainActivity.this, contactList,
+                R.layout.list_item, new String[]{"id", "name",
+                "birthPlace"}, new int[]{R.id.name,
+                R.id.email, R.id.mobile});
+
+        lv.setAdapter(adapter);
+    }
+
+}
 
 }
